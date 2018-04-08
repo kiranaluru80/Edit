353a0e544af702af26e742aee0e8bf6f932e65f1
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2018-04-08 00:17:23 -0400

    edit

---
+++ SampleDB-20Mar-2/SampleDB/JobInformationPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SampleDB
+{
+    public partial class JobInformationPage : ContentPage
+    {
+        public JobInformationPage()
+        {
+            
+            InitializeComponent();
+
+            resetRef.Clicked += (sender, e) =>
+            {
+                
+               //Navigation.PushAsync(new CommunicationsPage(12,"12345"));
+                Navigation.PushAsync(new EditCommunicationsPage(12, "12345"));
+            };
+        }
+    }
+}
